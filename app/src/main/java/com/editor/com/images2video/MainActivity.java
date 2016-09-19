888eb536582dfc72cba28a2752986cda7b31c1be
Umair_Adil <umair_adil@live.com>
2016-09-19 15:40:13 +0500

    Added Video and Audio trimmer, no UI.

--- app/src/main/java/com/editor/com/images2video/MainActivity.java
+++ app/src/main/java/com/editor/com/images2video/MainActivity.java
@@ -9,12 +9,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.editor.com.images2video.controller.AudioTrimmer;
 import com.editor.com.images2video.controller.Images2Movie;
+import com.editor.com.images2video.controller.VideoTrimmer;
 import com.editor.com.images2video.controller.callback.IConvertCallback;
 import com.editor.com.images2video.controller.model.AudioFormat;
 import com.editor.com.images2video.models.Collection;
 import com.editor.com.images2video.utils.Constants;
-import com.editor.com.images2video.utils.Utils;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Collection> listStore = new ArrayList<Collection>();
     private Collection collection;
 
-    Button btn_make_movie;
+    Button btn_make_movie, btn_trim_audio,btn_trim_video;
 
     File[] myImages;
 
@@ -42,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
         myImages = getMyCollection();
 
         btn_make_movie = (Button) findViewById(R.id.button);
+        btn_trim_audio = (Button) findViewById(R.id.button2);
+        btn_trim_video = (Button) findViewById(R.id.button3);
 
 
         btn_make_movie.setOnClickListener(new View.OnClickListener() {
@@ -50,12 +53,12 @@ public class MainActivity extends AppCompatActivity {
                 IConvertCallback callback = new IConvertCallback() {
                     @Override
                     public void onSuccess(File convertedFile) {
-                        Log.i(TAG,"Done: "+convertedFile.getName());
+                        Log.i(TAG, "Done: " + convertedFile.getName());
                     }
 
                     @Override
                     public void onFailure(Exception error) {
-                        Log.e(TAG,"Error: "+error);
+                        Log.e(TAG, "Error: " + error);
                     }
                 };
                 Toast.makeText(context, "Creating Movie...", Toast.LENGTH_SHORT).show();
@@ -64,6 +67,55 @@ public class MainActivity extends AppCompatActivity {
                         .setFormat(AudioFormat.MP3)
                         .setCallback(callback)
                         .convert();
+            }
+        });
+
+        btn_trim_audio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IConvertCallback callback = new IConvertCallback() {
+                    @Override
+                    public void onSuccess(File convertedFile) {
+                        Log.i(TAG, "Done: " + convertedFile.getName());
+                    }
+
+                    @Override
+                    public void onFailure(Exception error) {
+                        Log.e(TAG, "Error: " + error);
+                    }
+                };
+                Toast.makeText(context, "Trimming Audio...", Toast.LENGTH_SHORT).show();
+                File audio = new File(Environment.getExternalStorageDirectory() + File.separator + Constants.MY_FOLDER + File.separator + "sound.mp3");
+                AudioTrimmer.with(context)
+                        .setFile(audio)
+                        .setFormat(AudioFormat.MP3)
+                        .setCallback(callback)
+                        .trim();
+            }
+        });
+
+
+        btn_trim_video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IConvertCallback callback = new IConvertCallback() {
+                    @Override
+                    public void onSuccess(File convertedFile) {
+                        Log.i(TAG, "Done: " + convertedFile.getName());
+                    }
+
+                    @Override
+                    public void onFailure(Exception error) {
+                        Log.e(TAG, "Error: " + error);
+                    }
+                };
+                Toast.makeText(context, "Trimming Movie...", Toast.LENGTH_SHORT).show();
+                File video = new File(Environment.getExternalStorageDirectory() + File.separator + Constants.MY_FOLDER + File.separator + "video.mp4");
+                VideoTrimmer.with(context)
+                        .setFile(video)
+                        .setFormat(AudioFormat.MP3)
+                        .setCallback(callback)
+                        .trim();
             }
         });
 
